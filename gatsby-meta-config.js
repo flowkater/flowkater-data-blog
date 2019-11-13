4e893146fe0eb69d371a8b8dfff497310960c61f
flowkater <chjw102@gmail.com>
2019-11-13 13:04:56 +0900

    meta data update

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: `flowkater's data;`,
   description: `Blog posted about devlog, TIL, data, math, statistics.`,
   author: `Jaewoo Cho (flowkater)`,
-  introduction: `작은 스타트업 합니다. 항상 생존이 우선이지만 그래도 하고 싶은거 하는 기획자 겸 초보 개발자입니다. 아참, 데이터 과학 강의도 합니다.`,
+  introduction: `작은 스타트업 합니다. 항상 생존이 우선이지만 그래도 하고 싶은거 하는 기획자 겸 개발자입니다. 아참, 데이터 과학 강의도 합니다.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
